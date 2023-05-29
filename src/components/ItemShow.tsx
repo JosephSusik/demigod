36d02dcd78a3e73b431173d8605bab0054e28f4c
JosephSusik <joseph.susik@gmail.com>
2023-05-29 11:47:28 +0200

    tmp img

--- src/components/ItemShow.tsx
+++ src/components/ItemShow.tsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
 import './styles/ItemShow.css'
 
+import img from '../img/demigod-triko.jpeg'
+
 function ItemShow({text, link}:{text:string, link:string}) {
     return(
             <div className='item-show-item'>
                 <Link to={link}>
+                    <img src={img} alt="" />
                     <p className='text'>{text}</p>
                 </Link>
             </div>
