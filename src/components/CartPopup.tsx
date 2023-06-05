cc08e3f36769ab3081a9986c5946ae6725b473ca
JosephSusik <joseph.susik@gmail.com>
2023-06-05 10:29:09 +0200

    cart popup

--- src/components/CartPopup.tsx
+++ src/components/CartPopup.tsx
@@ -34,7 +34,34 @@ function CartPopup() {
                 </div>
             </div>
 
+            <div className='cart-item'>
+                <img src={img} alt="" />
+                <div className='info'>
+                    <p className='name'>Manipulate - Tričko</p>
+                    <p className='detail'>Barva: Béžová</p>
+                    <p className='detail'>Velikost: M</p>
+                    <p className='price'>549 Kč</p>
+                </div>
+                <div className='remove'>
+                    <CloseIcon className='icon'/>
+                </div>
+            </div>
+
+            <div className='cart-item'>
+                <img src={img} alt="" />
+                <div className='info'>
+                    <p className='name'>Manipulate - Tričko</p>
+                    <p className='detail'>Barva: Béžová</p>
+                    <p className='detail'>Velikost: M</p>
+                    <p className='price'>549 Kč</p>
+                </div>
+                <div className='remove'>
+                    <CloseIcon className='icon'/>
+                </div>
+            </div>
+
             <NavLink to={'/cart'}><button>Pokračovat do košíku</button></NavLink>
+
         </div>
     );
 }
